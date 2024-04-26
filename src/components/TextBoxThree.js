72d430a7e93f195032d6f28939005ef8de53d97e
pineonline10 <joshisstillpine@gmail.com>
2024-04-26 14:22:56 +0100

    fixes image two

---
+++ src/components/TextBoxThree.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Mario from '../img/mariokart.png'
+import { Parallax } from 'react-parallax'
+const TextBoxTwo = () => {
+  return (
+    <div className='text-box-container'>
+    <div className='text-box'>
+       <p>
+       P.I. places significant emphasis on developing compelling intellectual properties IPs, akin to Valve’s strategic creation of immersive game worlds. By fostering unique and engaging game narratives and environments, P.I. aspires to build a portfolio of games that not only captivates players but also establishes a foundation for long-term community engagement and expansion. This focus on IP development is central to P.I.'s vision, aiming to replicate the success and sustainability seen in Valve’s iconic franchises by leveraging community-driven content and innovative storytelling.
+
+        Understanding the power of strategic partnerships and the profound impact of marrying innovative gameplay with beloved characters, we are inspired by Velan Studios’ successful collaboration with Nintendo on Mario Kart Live: Home Circuit. This partnership not only leveraged Nintendo’s iconic IP but also demonstrated the profound impact of combining innovative gameplay with beloved characters, leading to significant commercial success, with over 1.73 million units sold.
+       </p>
+    </div>
+    <Parallax className='parallax-box' strength={200}>
+  <div style={{ height: '500px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <img src={Mario} alt="Mario Kart" style={{ width: '100%', maxWidth: '500px', height: 'auto' }} />
+  </div>
+</Parallax>
+    </div>
+    
+  )
+}
+
+export default TextBoxTwo
--- src/components/imageTwo.js
+++ src/components/imageTwo.js
@@ -2,7 +2,7 @@ import { Parallax } from 'react-parallax';
 import Flight from '../img/spacex-pnPS3Ox_2vE-unsplash.jpg'
 
 const ImageTwo = () => (
-    <Parallax className='image' bgImage={Flight} speed={-15} strength={200}>
+    <Parallax className='image' bgImage={Flight} speed={1.5} strength={200}>
      <div id='overview' className='content'>
         <span className='img-txt'>Company Vision & Overview</span>
      </div>
